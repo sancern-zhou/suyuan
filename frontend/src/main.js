f62d9f01df519beabadd723d622f15132be65d0a
root <root@localhost.localdomain>
2026-07-14 16:30:52 +0800

    feat: add standalone company login flow

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/index'
+import { useAuthStore } from './auth/authStore.js'
+import { installAuthGuard } from './auth/routerGuard.js'
 import './styles/main.scss'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
+installAuthGuard(router, useAuthStore(pinia))
 app.use(router)
 
 app.mount('#app')
