a960c3023b9f8625f802eb1c5b156bae0bdec143
root <root@localhost.localdomain>
2026-09-17 21:22:40 +0800

    test: default scheduled task execution storage to file in test env

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault("TASK_REVIEW_STORAGE", "file")
 os.environ.setdefault("SMART_EVENT_STORAGE", "file")
+os.environ.setdefault("SCHEDULED_TASK_EXECUTION_STORAGE", "file")
